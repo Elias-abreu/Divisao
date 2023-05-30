57b9b6ba15c4f42bd7f7ed315c6a7ecd95461102
Elias de Abreu <elias.silva@ifro.edu.br>
2023-05-30 16:29:35 -0300

    Tratamento de exceção

--- ConexaoBanco/Program.cs
+++ ConexaoBanco/Program.cs
@@ -1,9 +1,15 @@
 ﻿
+try
+{
+    Console.WriteLine("Informe o primeiro valor!");
+    int a = int.Parse(Console.ReadLine());
+    Console.WriteLine("Informe o segundo valor!");
+    int b = int.Parse(Console.ReadLine());
+    int result = a / b;
+    Console.WriteLine("O resultado é :"+result);
 
-Console.WriteLine("Informe o primeiro valor!");
-int a = int.Parse(Console.ReadLine());
-Console.WriteLine("Informe o segundo valor!");
-int b = int.Parse(Console.ReadLine());
-int result = a / b;
-Console.WriteLine(result);
+}catch(Exception ex)
+{
+    Console.WriteLine("Algo de errado não está certo!");
+}
 
